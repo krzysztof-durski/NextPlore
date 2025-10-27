434d8c650dca3f7d70252270bd4cf634a2a3ed9d
Krzysiek Durski <dursky.k@gmail.com>
2025-10-27 15:43:51 +0100

    feat: Define UserTag model with user_tag

--- src/models/location.js
+++ src/models/location.js
@@ -1,41 +1,41 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../db/database.js";
 
-const Location = sequelize.define("location",{
-    location_id:{
-        type: DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
-    },
-    fsq_place_id:{
-        type: DataTypes.STRING,
-        allowNull:false,
-        unique:true,
-    },
-    Name:{
-        type:DataTypes.STRING,
-        allowNull:false,
-    },
-    Address:{
-        type: DataTypes.STRING,
-        allowNull:false,    
-    },
-    location:{
-        type: DataTypes.GEOGRAPHY('POINT',4326),
-        allowNull:false,
-    },
-    Description:{
-        type: DataTypes.STRING,
-        allowNull:true,
-    },
-    Links:{
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        allowNull:true,
-    },
-    country_code:{
-        type: DataTypes.STRING,
-        allowNull:false,
-    }
-})
+const Location = sequelize.define("location", {
+  location_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  fsq_place_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  Name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  Address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  location: {
+    type: DataTypes.GEOGRAPHY("POINT", 4326),
+    allowNull: false,
+  },
+  Description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  Links: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: true,
+  },
+  country_code: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 export default Location;
