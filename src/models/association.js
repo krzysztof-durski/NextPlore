adb47742928eccebf041486b3018b5026d905cce
Adityuhh11 <saipremaditya@gmail.com>
2025-10-28 13:29:17 +0100

    refactor: remove UserTag and PlaceTag models; streamline many-to-many associations

--- src/models/association.js
+++ src/models/association.js
@@ -1,19 +1,11 @@
 import User from './user.js';
 import Tag from './tag.js';
 import Location from './location.js';
-import UserTag from './user_tag.js';
-import PlaceTag from './place_tag.js';
 
-// User and UserTag (Many-to-Many through UserTag)
-User.hasMany(UserTag, { foreignKey: 'userId' });
-UserTag.belongsTo(User, { foreignKey: 'userId' });
+// User and Tag (Many-to-Many)
+User.belongsToMany(Tag, { through: 'user_tag', foreignKey: 'userId' });
+Tag.belongsToMany(User, { through: 'user_tag', foreignKey: 'tagId' });
 
-Tag.hasMany(UserTag, { foreignKey: 'tagId' });
-UserTag.belongsTo(Tag, { foreignKey: 'tagId' });
-
-// Location and PlaceTag (Many-to-Many through PlaceTag)
-Location.hasMany(PlaceTag, { foreignKey: 'locationId' });
-PlaceTag.belongsTo(Location, { foreignKey: 'locationId' });
-
-Tag.hasMany(PlaceTag, { foreignKey: 'tagId' });
-PlaceTag.belongsTo(Tag, { foreignKey: 'tagId' });
+// Location and Tag (Many-to-Many)
+Location.belongsToMany(Tag, { through: 'place_tag', foreignKey: 'locationId' });
+Tag.belongsToMany(Location, { through: 'place_tag', foreignKey: 'tagId' });
