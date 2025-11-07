9649f658b0f32a53cd0840941e2dd4764deffe4c
Adityuhh11 <saipremaditya@gmail.com>
2025-11-07 11:40:46 +0100

    feat: implement initial locations controller with necessary imports

---
+++ src/controllers/locations.controller.js
@@ -0,0 +1,10 @@
+import Tag from "../models/tag.js";
+import Location from "../models/location.js";
+import User from "../models/user.js";
+import {asyncHandler} from "../utils/asynchandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import sequelize from "../db/database.js";
+
+
+
