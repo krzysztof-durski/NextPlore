c4b1393470421da3108f76393a815dd81b1756c3
Adityuhh11 <saipremaditya@gmail.com>
2025-11-14 22:37:15 +0100

    feat: add authentication middleware to locations routes

--- src/routes/locations.route.js
+++ src/routes/locations.route.js
@@ -1,6 +1,9 @@
 import { getnearbyLocations, getRecommendLocations , getPlaceDetails} from "../controllers/locations.controller.js";
 import express from "express";
+import { authenticate } from "../middleware/auth.middleware.js";
 const router = express.Router();
+
+router.use(authenticate);
 
 router.get("/", getnearbyLocations);
 router.post("/recommendations", getRecommendLocations);
